6485e733a320ac72c8601b8432d00f62437ba81f
swordarrow2 <2856986197@qq.com>
2018-07-17 18:35:34 +0800

    bug fixed

--- QRtools/app/src/main/java/com/meng/qrtools/MainActivity.java
+++ QRtools/app/src/main/java/com/meng/qrtools/MainActivity.java
@@ -3,6 +3,7 @@ package com.meng.qrtools;
 import android.app.*;
 import android.content.*;
 import android.os.*;
+import com.meng.mbrowser.tools.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		ExceptionCatcher.getInstance().init(this);
         setContentView(R.layout.main);
 		Intent i=new Intent(this,com.github.sumimakito.awesomeqrsample.MainActivity.class);
 		startActivity(i);
