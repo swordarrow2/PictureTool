203fc3e7fa6f8eaf08e5fc17db86f7a38c1ad143
swordarrow2 <2856986197@qq.com>
2018-08-06 17:35:26 +0800

    GIF解码部分完成

--- QRCodeReaderAndCreator/app/src/main/java/com/meng/qrtools/creator/gifAwesomeQr.java
+++ QRCodeReaderAndCreator/app/src/main/java/com/meng/qrtools/creator/gifAwesomeQr.java
@@ -28,17 +28,22 @@ import com.waynejo.androidndkgif.GifImageIterator;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class gifAwesomeQr extends Fragment {
 
     private boolean useDither = true;
     private ImageView imageView;
-    private Button btn_selectImg,btn_start_decode,
-            encode_gif_btn;
+    private Button btnSelectImg, btnStartDecode,
+            encodeGifBtn;
     private CheckBox lowMem, dither;
     private TextView imgPath;
     private final int SELECT_FILE_REQUEST_CODE = 8212;
-    private String selectGifPath="";
+    private String selectGifPath = "";
+    private Bitmap[] bitmaps;
+    private int gifDelay;
+    private String tmpFolder=Environment.getExternalStorageDirectory().getAbsolutePath() +
+            "/Pictures/QRcode/tmp/";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -53,25 +58,21 @@ public class gifAwesomeQr extends Fragment {
         lowMem = (CheckBox) view.findViewById(R.id.gif_qr_checkbox_low_memery);
         dither = (CheckBox) view.findViewById(R.id.gif_qr_checkbox_dither);
         imageView = (ImageView) view.findViewById(R.id.image_view);
-        btn_selectImg = (Button) view.findViewById(R.id.gif_qr_button_selectImg);
-        btn_start_decode=(Button)view.findViewById(R.id.gif_qr_button_startDecodeImg) ;
-        encode_gif_btn = (Button) view.findViewById(R.id.gif_qr_button_encode_gif);
-        imgPath=(TextView)view.findViewById(R.id.gif_qr_selected_path);
+        btnSelectImg = (Button) view.findViewById(R.id.gif_qr_button_selectImg);
+        btnStartDecode = (Button) view.findViewById(R.id.gif_qr_button_startDecodeImg);
+        encodeGifBtn = (Button) view.findViewById(R.id.gif_qr_button_encode_gif);
+        imgPath = (TextView) view.findViewById(R.id.gif_qr_selected_path);
 
 
-        btn_selectImg.setOnClickListener(new OnClickListener() {
+        btnSelectImg.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View p1) {
                 // TODO: Implement this method
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(intent, SELECT_FILE_REQUEST_CODE);
-
+                selectImage();
             }
         });
-        btn_start_decode.setOnClickListener(new OnClickListener() {
+        btnStartDecode.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View p1) {
@@ -80,7 +81,7 @@ public class gifAwesomeQr extends Fragment {
             }
         });
 
-        encode_gif_btn.setOnClickListener(new OnClickListener() {
+        encodeGifBtn.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View p1) {
@@ -184,7 +185,6 @@ public class gifAwesomeQr extends Fragment {
             gifEncoder.encodeFrame(bitmap, delayMs);
         }
         gifEncoder.close();
-
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -203,6 +203,7 @@ public class gifAwesomeQr extends Fragment {
         if (requestCode == SELECT_FILE_REQUEST_CODE && resultCode == getActivity().RESULT_OK && data.getData() != null) {
             try {
                 Uri imageUri = data.getData();
+                imgPath.setText(selectGifPath);
                 selectGifPath = ContentHelper.absolutePathFromUri(getActivity().getApplicationContext(), imageUri);
             } catch (Exception e) {
                 log.e(e);
@@ -210,62 +211,63 @@ public class gifAwesomeQr extends Fragment {
         } else if (resultCode == getActivity().RESULT_CANCELED) {
             Toast.makeText(getActivity().getApplicationContext(), "用户取消了操作", Toast.LENGTH_SHORT).show();
         } else {
-            //  selectImage();
+              selectImage();
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    private void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, SELECT_FILE_REQUEST_CODE);
+    }
+
     private void decodeGif(final String path) {
-        imgPath.setText(path);
         if (lowMem.isChecked()) {
-            imgPath.setText(path+"\n使用了低配置模式");
+            imgPath.setText(path + "\n使用了低配置模式");
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    final GifDecoder gifDecoder = new GifDecoder();
-                    final GifImageIterator iterator = gifDecoder.loadUsingIterator(path);
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (iterator.hasNext()) {
-                                GifImage next = iterator.next();
-                                if (null != next) {
-                                    imageView.setImageBitmap(next.bitmap);
-                                    imageView.postDelayed(this, next.delayMs);
-                                } else {
-                                    log.t("解码失败，可能不是GIF动态图");
-                                }
-                            } else {
-                                iterator.close();
+                    GifDecoder gifDecoder = new GifDecoder();
+                    GifImageIterator iterator = gifDecoder.loadUsingIterator(path);
+                    int flag = 0;
+                    DecimalFormat df=new DecimalFormat();
+                    String style = "000";//定义要显示的数字的格式
+                    df.applyPattern(style);// 将格式应用于格式化器
+                    while (iterator.hasNext()) {
+                        GifImage next = iterator.next();
+                        if (next != null) {
+                            try {
+                                QRCode.saveMyBitmap(tmpFolder + df.format(flag++) + ".png", next.bitmap);
+                            } catch (IOException e) {
+                                log.e(e);
                             }
+                            gifDelay = next.delayMs;
+                        } else {
+                            log.t("解码失败，可能文件损坏");
                         }
-                    });
+                    }
+                    iterator.close();
                 }
             }).start();
         } else {
-            imgPath.setText(path+"\n未使用低配置模式");
+            imgPath.setText(path + "\n未使用低配置模式");
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     final GifDecoder gifDecoder = new GifDecoder();
-                    final boolean isSucceeded = gifDecoder.load(path);
-                    getActivity().runOnUiThread(new Runnable() {
-                        int idx = 0;
-
-                        @Override
-                        public void run() {
-                            if (isSucceeded) {
-                                Bitmap bitmap = gifDecoder.frame(idx);
-                                imageView.setImageBitmap(bitmap);
-                                if (idx + 1 < gifDecoder.frameNum()) {
-                                    imageView.postDelayed(this, gifDecoder.delay(idx));
-                                }
-                                ++idx;
-                            } else {
-                                log.t("解码失败，可能不是GIF动态图");
-                            }
+                    if (gifDecoder.load(path)) {
+                        bitmaps = new Bitmap[gifDecoder.frameNum()];
+                        gifDelay = gifDecoder.delay(1);
+                        for (int i = 0; i < gifDecoder.frameNum(); i++) {
+                            bitmaps[i] = gifDecoder.frame(i);
+                            log.i("第" + i + "张解码成功");
                         }
-                    });
+                    } else {
+                        log.e("解码失败，可能不是GIF文件");
+                    }
+
                 }
             }).start();
         }
