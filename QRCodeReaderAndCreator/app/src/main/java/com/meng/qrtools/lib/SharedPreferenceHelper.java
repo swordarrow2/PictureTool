ba829624e05d4dfcd1397a7c619e510e6f557085
swordarrow2 <2856986197@qq.com>
2018-08-25 16:06:57 +0800

    布尔值方法

--- QRCodeReaderAndCreator/app/src/main/java/com/meng/qrtools/lib/SharedPreferenceHelper.java
+++ QRCodeReaderAndCreator/app/src/main/java/com/meng/qrtools/lib/SharedPreferenceHelper.java
@@ -10,13 +10,13 @@ import android.content.SharedPreferences;
 public class SharedPreferenceHelper{
 
     private SharedPreferences sp;
-    // private SharedPreferences.Editor editor;
+    private SharedPreferences.Editor editor;
     private Context context;
 
     public SharedPreferenceHelper(Context c,String name){
         context=c;
         sp=context.getSharedPreferences(name,0);
-        //    editor=sp.edit();
+        editor=sp.edit();
     }
 
     public boolean getBoolean(String key){
@@ -26,13 +26,15 @@ public class SharedPreferenceHelper{
     public boolean getBoolean(String key,boolean p1){
         return sp.getBoolean(key,p1);
     }
-   /*
+
     public void putBoolean(String key,Boolean value){
         editor=sp.edit();
         editor.putBoolean(key,value);
         editor.commit();
     }
-	public String getValue(String key){
+
+   /*
+    public String getValue(String key){
         return sp.getString(key,null);
     }
 	public String getValue(String key,String defaultValue){
