cfaf8515a1827e82c9f16d5555dec27712d70b4e
swordarrow2 <2856986197@qq.com>
2018-11-02 22:34:34 +0800

    使用时间戳为二维码命名的选项

--- QRCodeReaderAndCreator/app/src/main/java/com/meng/qrtools/MainActivity.java
+++ QRCodeReaderAndCreator/app/src/main/java/com/meng/qrtools/MainActivity.java
@@ -55,25 +55,25 @@ public class MainActivity extends Activity{
     public String getAwesomeQRPath(){
         File f=new File(awesomeQRPath);
         if(!f.exists()) f.mkdirs();
-        return awesomeQRPath+(new Date()).toString()+".png";
+        return awesomeQRPath+(sharedPreference.getBoolean("useTimeStamp")?(System.currentTimeMillis()/1000):new Date().toString())+".png";
     }
 
     public String getArbAwesomeQRPath(){
         File f=new File(arbAwesomeQRPath);
         if(!f.exists()) f.mkdirs();
-        return arbAwesomeQRPath+(new Date()).toString()+".png";
+        return arbAwesomeQRPath+(sharedPreference.getBoolean("useTimeStamp")?(System.currentTimeMillis()/1000):new Date().toString())+".png";
     }
 
     public String getGifAwesomeQRPath(){
         File f=new File(gifAwesomeQRPath);
         if(!f.exists()) f.mkdirs();
-        return gifAwesomeQRPath+(new Date()).toString()+".gif";
+        return gifAwesomeQRPath+(sharedPreference.getBoolean("useTimeStamp")?(System.currentTimeMillis()/1000):new Date().toString())+".gif";
     }
 
     public String getGifArbAwesomeQRPath(){
         File f=new File(gifArbAwesomeQRPath);
         if(!f.exists()) f.mkdirs();
-        return gifArbAwesomeQRPath+(new Date()).toString()+".gif";
+        return gifArbAwesomeQRPath+(sharedPreference.getBoolean("useTimeStamp")?(System.currentTimeMillis()/1000):new Date().toString())+".gif";
     }
 
     public String getGifPath(){
@@ -85,7 +85,7 @@ public class MainActivity extends Activity{
     public String getBarcodePath(String barcodeFormat){
         File f=new File(barcodePath);
         if(!f.exists()) f.mkdirs();
-        return barcodePath+barcodeFormat+"/"+(new Date()).toString()+".png";
+        return barcodePath+barcodeFormat+"/"+(sharedPreference.getBoolean("useTimeStamp")?(System.currentTimeMillis()/1000):new Date().toString())+".png";
     }
 
     public void doVibrate(long time){
