8c42f3efbd52e5ea2e5f3bfcbd613f01837c0b73
swordarrow2 <2856986197@qq.com>
2019-05-26 22:06:57 +0800

    ，，，

---
+++ picTools2/app/src/main/java/ocr/OcrMain.java
@@ -0,0 +1,35 @@
+package ocr;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.meng.picTools.R;
+
+public class OcrMain extends Fragment {
+
+    private TextView tv;
+
+    public OcrMain() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.text_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tv = (TextView) view.findViewById(R.id.aboutTextView);
+        welcome();
+    }
+
+    private void welcome() {
+        tv.setText("选择想要使用的功能吧");
+    }
+
+}
---
+++ picTools2/app/src/main/java/ocr/cardocr/lib/bean/BaseBean.java
@@ -0,0 +1,10 @@
+package ocr.cardocr.lib.bean;
+
+/**
+ * Created by liangjiangli on 2017/11/6.
+ */
+
+public class BaseBean {
+    public int errorcode;
+    public String errormsg;
+}
