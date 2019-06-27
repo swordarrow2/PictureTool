3d5d470abf737c6315d48e3cb888a7b3576c60d4
swordarrow2 <2856986197@qq.com>
2019-06-27 23:39:45 +0800

    一个小问题

--- picTools2/app/src/main/java/com/meng/picTools/MainActivity.java
+++ picTools2/app/src/main/java/com/meng/picTools/MainActivity.java
@@ -11,7 +11,7 @@ import com.meng.picTools.libAndHelper.*;
 
 public class MainActivity extends Activity {
 
-    private String url = null;
+    private CharSequence url = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +20,14 @@ public class MainActivity extends Activity {
         SharedPreferenceHelper.Init(this, "main");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             boolean readonly = getIntent().getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false);
-            url = getIntent().getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString();
+            url = getIntent().getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         } else {
             Intent intent = new Intent(this, MainActivity2.class);
             if (url != null) {
-                intent.putExtra("pixivUrl", url);
+                intent.putExtra("pixivUrl", url.toString());
             }
             startActivity(intent);
             overridePendingTransition(0, 0);
